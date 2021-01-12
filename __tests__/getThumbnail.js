ec70cbe0a7f1130d51505b3adb44850bae146240
Tarek Djebali <tarekdj@gmail.com>
2021-01-12 16:16:10 +0100

    Update getThumbnail.js

--- __tests__/getThumbnail.js
+++ __tests__/getThumbnail.js
@@ -1,18 +1,11 @@
 const getThumbnail = require('../index');
 
 test('Gets a vidoo title and thumb url', async () => {
-    
-    jest.setTimeout(10000);
-    try {
-        const result = await getThumbnail('YGfv28PtXvA');
-        expect.assertions(2);
-        expect(result.title).toBe('Can you solve this tricky JavaScript Array problem?');
-        // Seems like the sqp changes over time so we can't rely on a static value.
-        expect(result.url).toInclude('https://i.ytimg.com/an_webp/YGfv28PtXvA/mqdefault_6s.webp');
-    }
-    catch (err) {
-        console.error(err);
-        //expect(JSON.stringify(err)).toContain('TimeoutError');
-    }
+    const result = await getThumbnail('YGfv28PtXvA');
+    console.log('result', result);
+    expect.assertions(2);
+    expect(result.title).toBe('Can you solve this tricky JavaScript Array problem?');
+    // Seems like the sqp changes over time so we can't rely on a static value.
+    expect(result.url).toInclude('https://i.ytimg.com/an_webp/YGfv28PtXvA/mqdefault_6s.webp');
 
 });
