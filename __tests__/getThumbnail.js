a9a5f031a001fe9cc59ebb150ef69a6461db6430
Tarek Djebali <tarekdj@gmail.com>
2021-01-12 15:42:46 +0100

    Update getThumbnail.js

--- __tests__/getThumbnail.js
+++ __tests__/getThumbnail.js
@@ -1,8 +1,9 @@
 const getThumbnail = require('../index');
 
 test('Gets a vidoo title and thumb url', async () => {
-    expect.assertions(2);
     const result = await getThumbnail('YGfv28PtXvA');
+    console.log(result);
+    expect.assertions(2);
     expect(result.title).toBe('Can you solve this tricky JavaScript Array problem?');
     // Seems like the sqp changes over time so we can't rely on a static value.
     expect(result.url).toInclude('https://i.ytimg.com/an_webp/YGfv28PtXvA/mqdefault_6s.webp');
