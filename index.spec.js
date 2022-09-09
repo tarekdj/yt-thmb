95ed6982e927e0dcac44d20a30903ab17a88704b
Tarek Djebali <tarekdj@gmail.com>
2022-09-09 13:24:09 +0200

    Update index.spec.js

--- index.spec.js
+++ index.spec.js
@@ -2,8 +2,6 @@ const fetchThumbnail = require('./index')
 
 test('Gets a vidoo title and thumb url', async () => {
   const result = await fetchThumbnail('iPIo9uez40I')
-  expect.assertions(1)
-  console.log('--->', result);
   // Seems like the sqp changes over time so we can't rely on a static value.
   expect(result).toInclude('https://i.ytimg.com/an_webp/iPIo9uez40I/mqdefault_6s.webp')
 })
